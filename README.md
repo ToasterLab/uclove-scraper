29bb2af986f514a81e85fd6c480607491c32d752
Huey <leejinhuey@gmail.com>
2019-06-28 17:57:36 +0800

    barely working version up

---
+++ README.md
@@ -0,0 +1,28 @@
+# uclove-scraper
+
+download all posts from UCLove
+
+## To-Do
+
+* Fetch comments and reactions to post
+* Handle emoji
+  * https://mbasic.facebook.com/story.php?story_fbid=2087381988040021&id=1310505995727628
+* Handle posts with media
+  * https://mbasic.facebook.com/story.php?story_fbid=2088104147967805&id=1310505995727628
+  * https://mbasic.facebook.com/story.php?story_fbid=2087384401373113&id=1310505995727628
+  * https://mbasic.facebook.com/story.php?story_fbid=2087383164706570&id=1310505995727628
+  * https://mbasic.facebook.com/story.php?story_fbid=2087372864707600&id=1310505995727628
+  * https://mbasic.facebook.com/story.php?story_fbid=2087365518041668&id=1310505995727628
+  * https://mbasic.facebook.com/story.php?story_fbid=2087364381375115&id=1310505995727628
+* Handle non-English posts
+  * https://mbasic.facebook.com/story.php?story_fbid=2088103724634514&id=1310505995727628
+* Handle inline hashtags
+  * https://mbasic.facebook.com/story.php?story_fbid=2087380378040182&id=1310505995727628
+  * https://mbasic.facebook.com/story.php?story_fbid=2087372691374284&id=1310505995727628
+* Bug with multi-para handling
+  * https://mbasic.facebook.com/story.php?story_fbid=2087368158041404&id=1310505995727628
+  * https://mbasic.facebook.com/story.php?story_fbid=2087367481374805&id=1310505995727628
+  * https://mbasic.facebook.com/story.php?story_fbid=2087366034708283&id=1310505995727628
+* Bug with slash (/) handling
+  * https://mbasic.facebook.com/story.php?story_fbid=2087364224708464&id=1310505995727628
+  * https://mbasic.facebook.com/story.php?story_fbid=2087361104708776&id=1310505995727628
--- uclove/spiders/main.py
+++ uclove/spiders/main.py
@@ -1,11 +1,71 @@
 # -*- coding: utf-8 -*-
+import os
+import json
 import scrapy
 
 
 class MainSpider(scrapy.Spider):
     name = 'main'
-    allowed_domains = ['mobile.facebook.com']
-    start_urls = ['https://mobile.facebook.com/uclove/']
+    allowed_domains = ['mbasic.facebook.com']
+    base_url = "https://mbasic.facebook.com"
+    start_urls = [
+        f"{base_url}/login.php?next=https%3A%2F%2Fmbasic.facebook.com%2Fucllove"
+    ]
+
+    def parse_post(self, response):
+        post_text = ""
+        hashtags = []
+        for para in response.css("#root > #m_story_permalink_view p"):
+            text = para.css("p::text").get()
+            if text is None:  # assume is hashtag
+                hashtags.append(dict(
+                    text="".join(para.css("span::text").getall()),
+                    url=para.css("a").attrib.get('href')
+                ))
+                continue
+            post_text += text
+        yield(dict(
+            hashtags=hashtags,
+            text=post_text,
+            url=response.meta['url'],
+            timestamp=response.meta['timestamp']
+        ))
+
+    def build_post_url(self, post_meta):
+        return f"https://mbasic.facebook.com/story.php?story_fbid={post_meta['mf_story_key']}&id={post_meta['page_id']}"
+
+    def get_post_timestamp(self, post_meta):
+        return list(post_meta['page_insights'].values())[0]['post_context']['publish_time']
+
+    def parse_page(self, response):
+        for post in response.xpath("//div[starts-with(@id, 'u_0_')]"):
+            post_meta = post.attrib.get('data-ft')
+            if post_meta is None:
+                continue
+            post_meta = json.loads(post_meta)
+            yield response.follow(
+                url=self.build_post_url(post_meta),
+                callback=self.parse_post,
+                meta=dict(
+                    url=self.build_post_url(post_meta),
+                    timestamp=self.get_post_timestamp(post_meta)
+                )
+            )
+        next_page_url = response.css(
+            "#structured_composer_async_container .h a::attr(href)").get()
+        yield response.follow(
+            url=f"{MainSpider.base_url}{next_page_url}",
+            callback=self.parse_page
+        )
 
     def parse(self, response):
-        pass
+        email = os.getenv("FACEBOOK_EMAIL")
+        password = os.getenv("FACEBOOK_PASSWORD")
+        formdata = {
+            "pass": password,
+            "email": email,
+        }
+        return scrapy.FormRequest.from_response(response,
+                                                formid="login_form",
+                                                formdata=formdata,
+                                                callback=self.parse_page)
